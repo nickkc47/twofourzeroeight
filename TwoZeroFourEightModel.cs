b4da230e3ac9234ac3283f1bd572ac53a4742e85
I'M EARTH <I'M EARTH@DESKTOP-EQIQR2N>
2017-11-17 14:09:19 +0700

    FI LAB5

--- TwoZeroFourEightModel.cs
+++ TwoZeroFourEightModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace twozerofoureight //nickkkkkkkkkk
+namespace twozerofoureight //nickkkkkkkkkkk
 {
     class TwoZeroFourEightModel : Model
     {
